1c373e646ffdb3aaa6782aee089ef148abc2ec38
Muhamed Borovac <mborovac1@etf.unsa.ba>
2021-05-03 23:58:00 +0200

    Dodana prazna ASP.NET aplikacija za projekat

---
+++ Imunizacija21/Imunizacija21/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Imunizacija21.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

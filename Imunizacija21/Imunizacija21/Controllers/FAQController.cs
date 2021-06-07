65b037b2c96ba274d2eff795f26765dbe54966c3
ecivgin1 <ecivgin1@etf.unsa.ba>
2021-06-08 00:04:36 +0200

    Dodan FAQ kontroler

---
+++ Imunizacija21/Imunizacija21/Controllers/FAQController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Imunizacija21.Data;
+using Imunizacija21.Models;
+
+namespace Imunizacija21.Controllers
+{
+    public class FAQController : Controller
+    {
+        /*private readonly DataContext _context;*/
+
+        static List<Tuple<String, String>> FAQ = new List<Tuple<String, String>>()
+        {
+            new Tuple<string, string>("hihihi", "hahahahahaha")
+        };
+
+        public FAQController(DataContext context)
+        {
+            
+        }
+
+        // GET: FAQ
+        public IActionResult Index()
+        {
+            return View(FAQ);
+        }
+    }
+}
